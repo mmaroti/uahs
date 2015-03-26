72a449609961856bddc0c8e09c7929e43a991fab
Miklos Maroti <mmaroti@gmail.com>
2015-03-26 19:47:17 +0100

    ok, now it compiles

--- src/UnivAlg/Array.hs
+++ src/UnivAlg/Array.hs
@@ -18,5 +18,5 @@ data Array a = Array Shape ([Int] -> a)
 
 instance Show a => Show (Array a) where
 	show (Array ShapeOne f) = show (f [])
-	show (Array (ShapeProd n ProdOne) f) = List.intercalate List.Map (\
+--	show (Array (ShapeProd n ProdOne) f) = List.intercalate List.Map (\
 	show _ = undefined
