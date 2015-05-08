b3bdf1a09a4ace1d6acade115bc8fbdba1201b09
Miklos Maroti <mmaroti@gmail.com>
2015-05-08 22:33:04 +0200

    removed old stuff

--- src/UnivAlg/Array.hs
+++ src/UnivAlg/Array.hs
@@ -1,48 +1,101 @@
+module UnivAlg.Array where
 
-module UnivAlg.Array (Array, size, shape, generate, index, scalar, vector, matrix, stack, concat) where
-
-import qualified UnivAlg.Shape as Shape
+import qualified UnivAlg.Semiring as Semiring
 import qualified Data.Vector as Vector
-import Prelude hiding (concat)
 import Control.Exception (assert)
 
-data Array a = Array Shape.Shape (Vector.Vector a)
+data Array a = Array [Int] (Vector.Vector a)
 	deriving (Show, Eq)
 
+coord2pos :: [Int] -> [Int] -> Int
+coord2pos [] =
+	\xs -> case xs of
+		[] -> 0
+		_ -> error "too many coordinates"
+coord2pos (n : ns) =
+	let	m = product ns
+		f = coord2pos ns
+	in \xs -> case xs of
+		(y : ys) -> assert (0 <= y && y < n) y * m + f ys
+		_ -> error "too few coordinates"
+
+pos2coord :: [Int] -> Int -> [Int]
+pos2coord [] = \k -> assert (k == 0) []
+pos2coord (n : ns) =
+	let	m = product ns
+		f = pos2coord ns
+	in \k -> let (i, j) = divMod k m in assert (0 <= i && i < n) i : f j
+
+shape :: Array a -> [Int]
+shape (Array ns _) = ns
+
+dim :: Array a -> Int
+dim = length . shape
+
 size :: Array a -> Int
-size (Array s _) = Shape.size s
-
-shape :: Array a -> Shape.Shape
-shape (Array s _) = s
-
-generate :: Shape.Shape -> ([Int] -> a) -> Array a
-generate s f = Array s (Vector.generate (Shape.size s) (f . (Shape.coords s)))
+size (Array ns v) =
+	let m = product ns
+	in assert (Vector.length v == m) m
 
 index :: Array a -> [Int] -> a
-index (Array s v) xs = (Vector.!) v (Shape.index s xs)
+index (Array ns v) xs = (Vector.!) v (coord2pos ns xs)
+
+slice :: Array a -> Int -> Array a
+slice (Array [] _) = error "cannot slice a scalar"
+slice (Array (n : ns) v) = let m = product ns in
+	\i -> assert (0 <= i && i < n) Array ns (Vector.slice (i * m) m v)
+
+generate :: [Int] -> ([Int] -> a) -> Array a
+generate ns f = Array ns (Vector.generate (product ns) (f . pos2coord ns))
+
+array :: [Int] -> [a] -> Array a
+array ns as =
+	let v = Vector.fromList as
+	in assert (product ns == Vector.length v) Array ns v
 
 scalar :: a -> Array a
-scalar a = Array Shape.Scalar (Vector.singleton a)
+scalar a = Array [] (Vector.singleton a)
 
 vector :: [a] -> Array a
-vector as = Array (Shape.Stack (length as) Shape.Scalar) (Vector.fromList as)
+vector as = Array [length as] (Vector.fromList as)
 
 matrix :: [[a]] -> Array a
 matrix as = stack (map vector as)
 
 stack :: [Array a] -> Array a
 stack [] = error "cannot stack an empty list"
-stack as =
-	let
-		s = shape (head as)
-		vs = map (\(Array s2 v2) -> assert (s == s2) v2) as
-	in
-		Array (Shape.Stack (length vs) s) (Vector.concat vs)
+stack as@(b : _) = stack' (shape b) as
 
-concat :: [Array a] -> Array a
-concat as =
-	let
-		s = Shape.Concat (map shape as)
-		v = Vector.concat (map (\(Array _ v2) -> v2) as)
-	in
-		Array s v
+stack' :: [Int] -> [Array a] -> Array a
+stack' ns as =
+	let vs = map (\(Array ms w) -> assert (ms == ns) w) as
+	in Array (length vs : ns) (Vector.concat vs)
+
+instance Functor Array where
+	fmap f (Array ns v) = Array ns (fmap f v)
+
+projection :: Semiring.Semiring a => [Int] -> Int -> Array a
+projection ns i =
+	let	k = ns !! i
+		l = product (drop i ns)
+		ms = ns ++ [k]
+		f = \x -> (x `div` l) `mod` k
+		g = \x -> x `mod` k
+		h = \x -> if (f x) == (g x) then Semiring.one else Semiring.zero
+	in Array ms (Vector.generate (product ms) h)
+
+collate :: (a -> a -> a) -> Array a -> Array a -> Array a
+collate f (Array ns v) (Array ms w) =
+	let	k = product (tail ms)
+		l = (head ms) * k
+		g = \i -> f ((Vector.!) v (div i k)) ((Vector.!) w (mod i l))
+	in assert (last ns == head ms) Array (ns ++ tail ms) (Vector.generate (product ns * k) g)
+
+collapse :: (a -> a -> a) -> Int -> Array a -> Array a
+collapse = undefined
+
+compose :: (a -> a -> a) -> (a -> a -> a) -> Array a -> Array a -> Array a
+compose f g a b = collapse g (dim a - 1) (collate f a b)
+
+multiply :: Num a => Array a -> Array a -> Array a
+multiply a b = compose (*) (+) a b
